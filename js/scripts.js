615a12f9f9521ea4adfca07551ac26dcd56ec452
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-26 11:14:17 -0700

    finalized test 2 with console.log a random number on button click
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,16 +15,42 @@ function Player2(overallScore2, turnScore2) {
 }
 
 function playGame(){ 
-let random = Math.floor(Math.random() * 6) + 1;
+  let random = Math.floor(Math.random() * 6) + 1;
 // let random2 = Math.floor(Math.random() * 6) + 1;
   return random;
 }
 
+function addRoll() {
+  const array = [];
+  array.push(playGame());
+  let sum = 0;
+  // let oldSum;
+  // let newSum;
+  array.forEach(function(numberToAdd) {
+    let oldSum = sum;
+    sum = sum + numberToAdd;
+    let newSum = sum;
+});
+}
 
 
 $(document).ready(function() {
+  // $("#game").on("click", )
   
   let turn = playGame();
-  console.log(turn);
+  let turnTotal = addRoll();
+  $("button").click(function() {
+    console.log(playGame());
   // event.preventDefault();
+  });
 });
+
+
+
+
+
+// function attachContactListeners() {
+//   $("ul#contacts").on("click", "li", function() {
+//     console.log("The id of this <li> is " + this.id + ".");
+//   });
+// };
