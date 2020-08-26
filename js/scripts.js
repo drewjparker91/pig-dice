777f97b95272288b54fd530ce6b72a59f52699a8
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-26 10:47:16 -0700

    finalized test 1 with console.log a random number
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
+// Business Logic
+function Game(player1, player2) {
+  this.player1 = player1;
+  this.player2 = player2;
+}
+
+function Player1(overallScore, turnScore) {
+  this.overallScore = overallScore;
+  this.turnScore = turnScore;
+}
+
+function Player2(overallScore2, turnScore2) {
+  this.overallScore2 = overallScore2;
+  this.turnScore2 = turnScore2;
+}
+
+function playGame(){ 
+let random = Math.floor(Math.random() * 6) + 1;
+// let random2 = Math.floor(Math.random() * 6) + 1;
+  return random;
+}
+
+
+
 $(document).ready(function() {
   
-  
-  event.preventDefault();
+  let turn = playGame();
+  console.log(turn);
+  // event.preventDefault();
 });
