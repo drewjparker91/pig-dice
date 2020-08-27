29cd225194fc68164bb4f9f49a44a0e4ed77ec9e
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-27 11:19:32 -0700

    testing overall score stopping game after reaching 100 without success
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,31 +12,41 @@ Player.prototype.rollDice = function () {
   this.turnScore += this.roll;
 }
 
-// copy of the random.
-// Player.prototype.rollDice = function () { 
-//   let random = Math.floor((Math.random() * 6) + 1);
-// while(random > 1) {
-//   random = random = Math.floor((Math.random() * 6) + 1);
-// }
-// return random;
-//   this.roll = random;
-//   this.turnScore += this.roll;
-// }
+
 
 Player.prototype.addScore = function() {
-  this.overallScore += this.turnScore;
-  if (this.overallScore >= 100) { 
-return this.overallScore;
+  this.overallScore += this.turnScore 
+  if (this.overallScore >= 100) {
+    return true;
+  } else {}
 }
+
+Player.prototype.winner = function() {
+  for (i = 0; i < this.overallScore; i++) {
+    if (this.overallScore == 100) {
+      break
+      
+    }
+    
+  }
+  console.log("winner");
+  // console.log("we got it")
 }
+
 
 // User Interface Logic
 let player1 = new Player();
 let player2 = new Player();
 
 $(document).ready(function() {
+  // $("button#hold").attr("disabled", true);
+  // $("button#roll").attr("disabled", true);
+  // let player1Win = $("#winner");
+  // let string = Object.values(player1);
+  // player1.winner(string);
+  // player1Win.text(player1.winner);
   
-  $("#roll").click(function(event) {
+  $("button#roll").click(function(event) {
     event.preventDefault();
     let player1Roll = $("#player1Roll");
     let player1TurnScore = $("#player1TurnScore");
@@ -52,6 +62,10 @@ $(document).ready(function() {
       $("#output").text("Overall Score: " + player1.overallScore);
     
   }); 
+
+  if (player1.overallScore >= 100) {
+    console.log("winner");
+  } else {}
     // event.preventDefault();
   
   
