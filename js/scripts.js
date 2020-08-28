ca05cc9694231845578d3ad6294a357890699bd8
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-27 21:37:30 -0700

    finished disabling buttons on win and disabling buttons on rolling a one. Added image to front page and added buttons to start and restart the game

--- js/scripts.js
+++ js/scripts.js
@@ -12,41 +12,12 @@ Player.prototype.rollDice = function () {
   this.turnScore += this.roll;
 }
 
-
-
 Player.prototype.addScore = function() {
   this.overallScore += this.turnScore 
-  // if (this.overallScore >= 100) {     
-  //   console.log("winner");
-  //   return true;
-  // } else {}
 }
 
-
-// copy addScore
-
-// Player.prototype.addScore = function() {
-//   this.overallScore += this.turnScore 
-//   let over = this.overallScore;
-//   for (let i = 0; i < over; i++) {
-//   if (over[i] >= 100) {
-//     break     
-//     // console.log("winner");
-    
-//   } else {
-//     ("Try again")
-// }
-// }
-// }
-
 Player.prototype.winner = function() {
-  for (i = 0; i < this.overallScore; i++) {
-    if (this.overallScore == 100) {
-      break 
-    }    
-  }
-  console.log("winner");
-  // console.log("we got it")
+ 
 }
 
 
@@ -55,58 +26,87 @@ let player1 = new Player();
 let player2 = new Player();
 
 $(document).ready(function() {
-  // $("button#hold").attr("disabled", true);
-  // $("button#roll").attr("disabled", true);
-  // let player1Win = $("#winner");
-  // let string = Object.values(player1);
-  // player1.winner(string);
-  // player1Win.text(player1.winner);
-  
+
   $("button#roll1").click(function(event) {
     event.preventDefault();
+    $("#loseTurn").hide();
     let player1Roll = $("#player1Roll");
     let player1TurnScore = $("#player1TurnScore");
     let string = Object.values(player1);
     player1.rollDice(string);
     player1Roll.text(player1.roll);
     player1TurnScore.text(player1.turnScore);
+    if (player1.roll === 1) {
+      $("button#hold1").attr("disabled", true);
+      $("button#roll1").attr("disabled", true);
+      $("button#hold2").removeAttr("disabled", true);
+      $("button#roll2").removeAttr("disabled", true);
+      player1.turnScore = 0;
+      $("#loseTurn").show();
+    } else { }
   }); 
-
   $("button#hold1").click(function(event) {
     event.preventDefault();
     player1.addScore(Object.values(player1));
     $("#output1").text("Overall Score: " + player1.overallScore);
-
+    $("button#hold1").attr("disabled", true);
+    $("button#roll1").attr("disabled", true);
+    $("button#hold2").removeAttr("disabled", true);
+    $("button#roll2").removeAttr("disabled", true);
+    player1.turnScore = 0;
+    
     if (player1.overallScore >= 100) {     
       $("button#hold1").attr("disabled", true);
       $("button#roll1").attr("disabled", true);
-      $("#winner").text("You are the Winner!");
+      $("button#hold2").attr("disabled", true);
+      $("button#roll2").attr("disabled", true);
+      $("#winner").text("Player 1, You are the Winner!");
     } else {}
   }); 
 
   $("button#roll2").click(function(event) {
     event.preventDefault();
+    $("#loseTurn").hide();
     let player2Roll = $("#player2Roll");
     let player2TurnScore = $("#player2TurnScore");
     let string = Object.values(player2);
     player2.rollDice(string);
     player2Roll.text(player2.roll);
     player2TurnScore.text(player2.turnScore);
+    if (player2.roll === 1) {
+      $("button#hold2").attr("disabled", true);
+      $("button#roll2").attr("disabled", true);
+      $("button#hold1").removeAttr("disabled", true);
+      $("button#roll1").removeAttr("disabled", true);
+      player2.turnScore = 0;
+      $("#loseTurn").show();
+    } else {}
   }); 
 
   $("button#hold2").click(function(event) {
     event.preventDefault();
     player2.addScore(Object.values(player2));
     $("#output2").text("Overall Score: " + player2.overallScore);
-
+    $("button#hold2").attr("disabled", true);
+    $("button#roll2").attr("disabled", true);
+    $("button#hold1").removeAttr("disabled", true);
+    $("button#roll1").removeAttr("disabled", true);
+    player2.turnScore = 0;
     if (player2.overallScore >= 100) {     
       $("button#hold2").attr("disabled", true);
       $("button#roll2").attr("disabled", true);
-      $("#winner").text("You are the Winner!");
+      $("button#hold1").attr("disabled", true);
+      $("button#roll1").attr("disabled", true);
+      $("#winner").text("Player 2, You are the Winner!");
     } else {}
   }); 
   
-    // event.preventDefault();
-  
-  
+  $("#playGame").click(function(event) {
+    $("#game").show();
+    $("#playGame").hide();
+    $("#restartGame").show();
+  });
+  $("#restartGame").click(function(event) {
+    location.reload();
+  });
 });
